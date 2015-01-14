57237027c2072688531826d5b2011b88b83226b1
Fabio Paes Pedro <dev@fliperamma.com>
2015-01-14 18:19:37 -0200

    Update FTools.cs

--- FTools.cs
+++ FTools.cs
@@ -35,7 +35,7 @@ public static class FTools
 				}
 				else
 				{
-					output = string.Concat(output, delimiter, " * ";	
+					output = string.Concat(output, delimiter, " * ");	
 				}
 			}
 		}
