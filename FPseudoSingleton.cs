e016b441aebaf239f265d3d5cc451655bc656274
Fabio Paes Pedro <fabio.paes@mktvirtual.com.br>
2015-11-06 15:20:01 -0200

    Pseudo Singleton base class using Generics

---
+++ FPseudoSingleton.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Pseudo singleton.
+/// Generic Type extends MonoBehaviour and T also must extend MonoBehaviour
+/// @author Fabio Paes Pedro
+/// </summary>
+public class FPseudoSingleton<T> : MonoBehaviour where T : MonoBehaviour
+{
+	#region PseudoSingleton
+
+	private static T _instance;
+	/// <summary>
+	/// Returns a static reference of this instance
+	/// </summary>
+	/// <value>The instance.</value>
+	public static T Instance {
+		get {
+			return _instance;
+		}
+	}
+
+	/// <summary>
+	/// Applying the static reference
+	/// </summary>
+	void Awake ()
+	{
+		if (_instance != null) {	
+			Debug.LogWarning ("There is another instance of " + this + " already. Destroying the new one");
+			Destroy (this);
+			return;
+		}
+		_instance = this as T;
+	}
+
+	/// <summary>
+	/// If the object is the original instance, nullify the static reference
+	/// </summary>
+	void OnDestroy ()
+	{
+		if (this == _instance) {
+			_instance = null;
+		}
+	}
+	#endregion
+}
+
